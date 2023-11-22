510e438c376e9ff243e29e89145f49dfd6ff07bb
N8 <nathan.brachotte+github@gmail.com>
2023-11-22 14:43:26 +0100

    feat: 🎸 add prisma typesafe example

---
+++ prisma/migrations/20231122133652_add_avatar/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Avatar" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Avatar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Avatar_userId_key" ON "Avatar"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Avatar" ADD CONSTRAINT "Avatar_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+export const userPayload = {
+  where: {
+    avatar: {
+      isNot: null,
+    },
+  },
+  include: {
+    avatar: true,
+  },
+};
+
+const userWithPost = Prisma.validator<Prisma.UserDefaultArgs>()(userPayload);
+
+export type UserWithPost = Prisma.UserGetPayload<typeof userWithPost>;
 
 @Injectable()
 export class AppService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getFirstUserName(): Promise<string> {
-    const user = await this.prisma.user.findFirst();
+    const user = await this.prisma.user.findFirst(userWithPost);
 
     return user.name;
   }
